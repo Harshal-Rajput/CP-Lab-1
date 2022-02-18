453050d67450d88ab57072e0d9ce07db9315fc6d
Harshal Rajput <99942915+Harshal-Rajput@users.noreply.github.com>
2022-02-18 10:57:57 +0530

    Create Sum_of_n_Numbers.c

---
+++ Sum_of_n_Numbers.c
@@ -0,0 +1,14 @@
+//WAP to print sum of n natural no.
+
+#include<stdio.h>
+int main()
+{
+    int i,x,n;
+    printf("Enter the value of n\n");
+    scanf("%d", &n);
+    x==0;
+    for(i=1;i<=n;i++)
+    {
+        x=x+i;
+    }
+    printf("Sum of the first %d natural numbers is %d\n", n,x);
